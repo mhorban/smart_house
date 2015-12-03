01142a41cc13e84cd9ff21dac3737e2255a531b9
Marian Horban <horbanmarian@gmail.com>
2015-12-04 01:00:21 +0200

    CalendarTask - init method

--- smart_house/periodic_calendar_task.py
+++ smart_house/periodic_calendar_task.py
@@ -1,3 +1,26 @@
+
+class CalendarTask(object):
+    """
+    each N period - mean start now and continue forever
+    each N period start XX.XX.XXXX - mean start XX.XX.XXXX do forever
+    X times each N period start XX.XX.XXXX - mean X times start XX.XX.XXXX with period N
+    X times each N period start XX.XX.XXXX end YY.YY.YYYY - mean X times start XX.XX.XXXX  end YY.YY.YYYY with period N
+    once - mean now once
+    once start XX.XX.XX
+
+    if period is more than 30 sec we use CalendarTask
+    """
+    def __init__(self, count=-1, period=5, start=None, end=None):
+        """
+
+        :param count: -1 means forever
+        :param period: N sec
+        :param start: datetime, None mean now
+        :param end: datetime, None mean forever
+        """
+
+
+
 def wait_until(execute_it_now):
     while True:
         diff = (execute_it_now - datetime.now()).total_seconds()
